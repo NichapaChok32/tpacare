d9d13c6d22075ed97a8dc5391b9b28cb0dd6da64
Nichapa Chokaree <nichapachok32@gmail.com>
2020-12-30 17:37:40 +0700

    set to staging

--- plugins/line-liff.js
+++ plugins/line-liff.js
@@ -6,7 +6,7 @@ Vue.use(liff)
 export default (context, inject) => {
   const env = process.env.NODE_ENV
   const liffId =
-    env === 'production' ? '1654878951-DRO02K3M' : '1654878951-DRO02K3M'
+    env === 'production' ? '1654474751-g2Vmnrk3' : '1654474751-g2Vmnrk3'
   liff.init({ liffId })
   context.$liff = liff
   inject('liff', liff)
